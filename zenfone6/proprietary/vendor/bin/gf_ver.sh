c16d57ac0a816863197da0341c86ead0a1eb9736
xdesniy <58408600+xdesniy@users.noreply.github.com>
2024-05-30 20:18:12 +0300

    Delete zenfone6 directory

--- zenfone6/proprietary/vendor/bin/gf_ver.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-GF_VER_PACK=`getprop vendor.goodix.version.pack`
-
-setprop fp.version.driver "$GF_VER_PACK"
--- zenfone6/proprietary/vendor/bin/savelogs_complete.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-uts=`getprop persist.vendor.asus.uts`
-savelogs_prop=`getprop persist.vendor.asus.savelogs`
-am broadcast -a $uts -n com.asus.loguploader/.logtool.LogtoolReceiver --ei logtype $savelogs_prop
-echo "savelogs_complete Done"
