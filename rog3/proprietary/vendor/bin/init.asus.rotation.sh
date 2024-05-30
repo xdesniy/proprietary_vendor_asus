c90a546b6cfcca739586d4f92e1ce0e5c6a4ce00
xdesniy <58408600+xdesniy@users.noreply.github.com>
2024-05-30 20:17:29 +0300

    Delete rog3 directory

--- rog3/proprietary/vendor/bin/init.asus.rotation.sh
+++
@@ -1,8 +0,0 @@
-#!/vendor/bin/sh
-rotation_type=`getprop sys.screen.rotation`
-echo "rotation_type : $rotation_type" > /dev/kmsg
-
-echo $rotation_type> sys/devices/platform/goodix_ts.0/rotation_type
-
-
-
