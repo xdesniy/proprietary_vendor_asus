585504c3f40f5d101c32a2088cfceb8f086bc354
xdesniy <58408600+xdesniy@users.noreply.github.com>
2024-05-30 20:14:21 +0300

    Delete rog3 directory

--- rog3/proprietary/vendor/bin/mount_apd.sh
+++
@@ -1,8 +0,0 @@
-#! /vendor/bin/sh
-chown -R system:system /APD
-chmod -R 0775 /APD
-
-chown -R system:system /ADF
-chmod -R 0775 /ADF
-
-
--- rog3/proprietary/vendor/etc/get_dt_mac.sh
+++
@@ -1,8 +0,0 @@
-type=`getprop vendor.asus.dongletype`
-if [ "$type" == "3" ]; then
-	dt_mac=`cat /sys/class/net/eth0/address`
-	#echo "[GetDtMAC] dt_mac = $dt_mac"
-	setprop vendor.oem.asus.dtid $dt_mac
-else
-	exit 0
-fi
